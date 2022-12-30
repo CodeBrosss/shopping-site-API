488e4b89f6de521b3724d1c8a4d37c21e94a314f
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-12-30 18:44:45 +0100

    minor fix

--- routes/paystack.js
+++ routes/paystack.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { makePayment, verifyPayment } = require("../controllers/paystack.controller");
+const {
+    checkIfLoggedIn,
+    getHeaderToken,
+} = require("../controllers/auth.controller")
 
-router.route("/pay").post(makePayment);
+router.route("/pay").post(
+    getHeaderToken,
+    checkIfLoggedIn,
+    makePayment
+);
 router.route("/callback").get(verifyPayment);
 
 module.exports = router;
